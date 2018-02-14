caa7c175efdb10aeb9d4b5f8a5f3ad830d6b6395
screx <codyreceno@gmail.com>
2018-02-14 16:46:41 -0500

    refactoring/testing/abstracting

--- cwl_workflow.py
+++ cwl_workflow.py
@@ -4,49 +4,6 @@
 
 # trying to develop a tool that allows users to generate workflows
 # add something to core_config
-class CommandLineTool:
-	"""A CommandLineTool object is used to interact with CWL tool descriptions
-	in order to write workflows. It should be able to collect all the necessary inputs
-	and use them to combine command line tools into a workflow."""
-	def __init__(self, toolname):
-		self.toolname = toolname
-		# try:
-		# 	self.base_dir = config.param("cwl", "cwl_base")
-		# 	self.filename = config.param("cwl", toolname)
-		# except Exception e:
-		# 	print("{toolname} doesnt exist. Check the config file and try again"\
-		# 		.format(toolname))
-		self.inputs = {}
-		self.outputs = {}
-		self.cwl = parse_cwl(self)
-
-	def parse_cwl(self):
-		# REQUIREMENTS: get_path() has to point at a real cwl file in yml format. 
-		with open(self.get_path()) as info:
-			info_dict = yaml.load(info)
-		self.inputs = info_dict["inputs"]
-		self.outputs = infor_dict["outputs"]
-		return info_dict
-
-	def add_input(self, input_name, input_value):
-		# REQUIRES: the input has to exist in the cwl file.
-		if input_name in self.inputs:
-			self.inputs[input_name]["value"] = input_value
-		else:
-			print "{input_name} not in inputs".format(input_name)
-
-	def get_path(self):
-		return self.base_dir + "/" + self.filename
-
-	def get_valid_inputs(self):
-		valid_inputs = {}
-		for param in self.inputs:
-			if "value" in param:
-				valid_inputs[param] = self.inputs[param]
-		return valid_inputs
-
-	def get_valid_outputs(self):
-		return self.outputs # they are all valid and can be parsed later.
 
 class Workflow:
 	def __init__(self):
@@ -79,7 +36,7 @@ class Workflow:
 
 		workflow = {
 			'cwlVersion': cwl_version,
-			'class': 'Workflow'
+			'class': 'Workflow',
 			'inputs': inputs,
 			'outputs': outputs,
 			'steps': steps,
@@ -99,11 +56,12 @@ class Workflow:
 		inputs = {}
 		for i in self.steps:
 			curstep = self.steps[i]
-			for j in curstep["in"]
+			for j in curstep["in"]:
 				curparam = curstep["in"][j]
 				if isinstance(curparam["value"], dict):
 					# chainable types
-				else:			
+					pass
+				else:
 					key = "{toolname}_{parametername}".format(i, j)
 					inputs[key]["type"] = curparam["type"]
 					if "secondary_files" in curparam:
@@ -126,39 +84,46 @@ class Workflow:
 					source = "{toolname}_{parametername}".format(tool, j)
 					cwl_steps[tool][param] = source
 				else:
+					pass
 			for param in self.steps[tool]["out"]:
 				out += [param]
 		return cwl_steps
 
 	def build_outputs(self):
-		outputs = {'out': []}
-		for tool in self.steps:
-
-		# TODO: add the outputs to the workflow file
-		# how do we know what outputs we want?
-
-# class PicardSamToFastq(CommandLineTool):
-# 	def __init__(self):
-# 		# TODO make an init
-# 		# need a base dir for all CWL files
-# 		# get _inputs will add all the important values to this class
-# 		self.parse_cwl(cwl_dir, "picard_sam_to_fastq.cwl")
-
-# 	inputs["illumina_clip"]["value"] = adapter_file + config.param("trimmomatic","illuminal_clip_settings") + headcrop_length
+		outputs = {"out": []}
+		steps = self.steps
+		for tool in steps:
+			curstep = self.steps["out"]
+			for param in curstep:
+				outputs["out"] = param
 
 
-	# actual descriptions of the tool.	
-	# how do i actually want to keep track of and use these tools.
 
-	# What oop programming techniques can i/should i use.
+			# TODO: add the outputs to the workflow file
+			# how do we know what outputs we want?
+
+	# class PicardSamToFastq(CommandLineTool):
+	# 	def __init__(self):
+	# 		# TODO make an init
+	# 		# need a base dir for all CWL files
+	# 		# get _inputs will add all the important values to this class
+	# 		self.parse_cwl(cwl_dir, "picard_sam_to_fastq.cwl")
+
+	# 	inputs["illumina_clip"]["value"] = adapter_file + config.param("trimmomatic","illuminal_clip_settings") + headcrop_length
 
 
-# in my run steps I can have it so that it can just 
-# have a .add(workflow) method which adds it to the workflow
+		# actual descriptions of the tool.	
+		# how do i actually want to keep track of and use these tools.
 
-# what else do I need?
-# need a way to interact with the actual pipeline now
-# this might prove to be a bit difficult
-# the pipeline has methods/functions to get data from the config and readme files
-# also to select inputs based on priority
-# need to also hard code some params in.
+		# What oop programming techniques can i/should i use.
+
+
+	# in my run steps I can have it so that it can just 
+	# have a .add(workflow) method which adds it to the workflow
+
+	# what else do I need?
+	# need a way to interact with the actual pipeline now
+	# this might prove to be a bit difficult
+	# the pipeline has methods/functions to get data from the config and readme files
+	# also to select inputs based on priority
+	# need to also hard code some params in.
